ebf9e0592075c532032e1779aa9e652b15389616
Mathieu Brochard <mathieu.brochard@outlook.com>
2020-05-18 09:57:37 +0200

    Add NAT Gateway tagging

--- ec2-owner-autotag/index.js
+++ ec2-owner-autotag/index.js
@@ -1,14 +1,48 @@
 const aws = require('aws-sdk')
 const ec2 = new aws.EC2()
 
+const handleRunInstances = async (event, userName) => {
+  const getInstancesVolumes =  async instanceIds => {
+    const result = await ec2.describeVolumes({
+      Filters: [{
+        Name: 'attachment.instance-id',
+        Values: instanceIds
+      }]
+    }).promise()
+  
+    return result.Volumes.map(v => v.VolumeId)
+  }
+
+  const instanceIds = event['detail']['responseElements']['instancesSet']['items'].map(i => i.instanceId)
+  const volumeIds = await getInstancesVolumes(instanceIds)
+
+  return [...instanceIds, ...volumeIds]
+}
+
+const handleCreateNatGateway = async (event, userName) => {
+  return [event['detail']['responseElements']['CreateNatGatewayResponse']['natGateway']['natGatewayId']]
+}
+
 exports.handler = async (event, _) => {
   console.log(JSON.stringify(event))
 
-  const instanceIds = event['detail']['responseElements']['instancesSet']['items'].map(i => i.instanceId)
+  const eventHandleMapping = [{
+    'RunInstances': handleRunInstances,
+    'CreateNatGateway': handleCreateNatGateway
+  }]
+
+  const eventName = event['detail']['eventName']
   const userName = event['detail']['userIdentity']['userName']
 
+  let resourceIds = []
+  if (eventName === 'RunInstances') {
+    resourceIds = await handleRunInstances(event, userName)
+  } else if (eventName === 'CreateNatGateway') {
+    resourceIds = await handleCreateNatGateway(event, userName)
+  }
+
   await ec2.createTags({
-    Resources: instanceIds,
+    Resources: resourceIds,
     Tags: [{
       Key: 'Owner',
       Value: userName
